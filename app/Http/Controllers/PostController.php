5181f1615a5ecac71e289b5ab7db37c2a43400f5
KrulH <mehmetcelik4@gmail.com>
2016-04-12 10:10:27 +0300

    Post Validations & Outout Errors

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,14 @@ class PostController extends Controller
 {
     public function postCreatePost(Request $request)
     {
-        $request->user()->posts()->save(new Post($request->all()));
-        return redirect()->route('dashboard');
+        $this->validate($request, [
+            'body' => 'required|max:1000'
+        ]);
+        $message = 'There was an error!';
+        if($request->user()->posts()->save(new Post($request->all())))
+        {
+            $message = 'Post successfully created!';
+        };
+        return redirect()->route('dashboard')->with(['message' => $message]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,7 @@
 @endsection
 
 @section('content')
-    @if(count($errors) > 0)
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4 error">
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{$error}}</li>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
-    @endif
+   @include('includes.message-block')
     @if(Session::has('message'))
         <div class="row">
             <div class="col-md-4 col-md-offset-4 success">
