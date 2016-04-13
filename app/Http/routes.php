6b7aac8d82375379fe7fa96c5ed24dc6d767b3a6
KrulH <mehmetcelik4@gmail.com>
2016-04-13 14:18:35 +0300

    Update DB and View

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -46,7 +46,8 @@ Route::get('/delete-post/{post_id}', [
     'as' => 'post.delete'
 ])->middleware('auth');
 
-Route::post('/edit', function(\Illuminate\Http\Request $request){
-return response()->json(['message' => $request['postId']]);
-})->name('edit');
+Route::post('/edit', [
+    'uses' => 'PostController@postEditPost',
+    'as' => 'edit'
+]);
 
